47517f70c4f5bbcc85f9720322e733f72269461e
Frances Reagan James <adtgwen@gmail.com>
2025-07-29 18:36:00 -0400

    adding more to the useEffect function

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Dashboard from "./pages/Dashboard";
 import CourseDetail from "./pages/CourseDetail";
+import { set } from "mongoose";
 
 
 function App() {
@@ -26,5 +27,31 @@ function App() {
     }
   },[])
   
+  const getCurrentUser = async() => {
+
+    try {
+      const response = await axios.get("/api/users/me")
+      setUser(response.data);
+    } catch (error) {
+      // token may be expired//
+      localStorage.removeItem("token");
+      delete axios.defaults.headers.common["Authorization"];
+    }
+  }
+
+  const login = async (email, password) => {
+    const response = await axios.post("/api/users/login", { email, password })
+    const { token, user: userData } = response.data
+
+    localStorage.setItem("token", token)
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
+    setUser(userData)
+
+    return userData
+  }
+
+  
+
+
 }
 
