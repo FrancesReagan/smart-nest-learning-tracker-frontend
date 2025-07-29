81363018dd0946cf3eeedb87876ad9ed630847c6
Frances Reagan James <adtgwen@gmail.com>
2025-07-29 18:31:20 -0400

    adding App.jsx imports and main App function states and backend connection--and check if user is logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import { useEffect, useState } from 'react'
-import axios from "axios"
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import axios from "axios";
+import AuthContext from "./contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NavBar from "./components/NavBar";
+import "./App.css"
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Dashboard from "./pages/Dashboard";
+import CourseDetail from "./pages/CourseDetail";
 
-
-import './App.css'
 
 function App() {
-  const []
- 
+  const [user, setUser] = useState(null);
 
-  return (
-    <>
-     
-      <h1>SMART NEST LEARNING TRACKER</h1>
-    
-</>
-  )
+  // backend connection//
+  axios.defaults.baseURL="http://localhost:3000";
+
+  // check if user is already logged in//
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}` getCurrentUser();
+    }
+  },[])
+  
 }
-export default App
+
