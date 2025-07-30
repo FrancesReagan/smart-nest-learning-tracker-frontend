df140e45bb1432d7e5ef8272bf59c05d95e2b6ae
Frances Reagan James <adtgwen@gmail.com>
2025-07-30 13:07:04 -0400

    adding coursecard function to coursecard

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
+
 function CourseCard({ course, onDelete, onEdit }) {
   const handleDelete = () => {
-    if
+    if(window.confirm("Do you want to delete this Course?")) {
+      onDelete(course._id);
+    }
+  }
+
+  const getStatusColor = (status) => {
+    switch (status) {
+      case "On the horizon": return "bg-yellow-500"
+      case "Working it": return "bg-blue-500"
+      case "BAM did it": return "bg-green-500"
+      default: return "bg-gray-500"
+    }
   }
 }
