18837eb22b1866cd02f8cf59936c7c3bd12787e6
Frances Reagan James <adtgwen@gmail.com>
2025-07-30 01:54:23 -0400

    working on LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -25,7 +25,50 @@ const handleSubmit = async (e) => {
 return (
   <div className="min-h-screen relative">
     {/* Birds Background */}
-    <div
+    <div className="absolute inset-0 bg-cover bg-center opacity-40"
+    style={{ backgroundImage: "url(/designer-5.jpg)"}}
+    />
+    <div className="absolute inset-0 bg-black/50" />
+    <div className="relative z-10 min-h-screen flex items-center justify-center px-6">
+      <div className="bg-white/10 backdrop-blur-sm p-8 rounded-lg w-full max-w-md">
+      <h2 className="text-2xl font-bold text-white text-center mb-6">Login</h2>
+      
+      {error && (
+        <div className="bg-red-500/20 border border-red-500 text-red-200 p-3 rounded mb-4">
+          {error}
+      </div>
+      )}
+
+ <form onSubmit={handleSubmit}>
+  <div className="mb-4">
+    <label className="block text-white mb-2">Email</label>
+    <input
+      type="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      className="w-full p-3 rounded bg-white/20 text-white placeholder-gray-300"
+      placeholder="your@email.com"
+      required
+      />
+  </div>
+
+ <div className="mb-6">
+  <label className="block text-white mb-2">Password</label>
+  <input
+   type="password"
+   value={password}
+   onChange={(e) => setPassword(e.target.value)}
+   className="w-full p-3 rounded bg-white/20 text-white placeholder-gray-300"
+   placeholder="Your Password"
+   />
+ </div>
+
+<button type="submit" className="w-full bg-blue-600 text-white py-3 rounded hover:bg-blue-700 mb-4" >
+  Login
+</button>
+ </form>
+
+    </div>
   </div>
 )
 
