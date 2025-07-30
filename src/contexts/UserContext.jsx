0050d2e1bfe107b03673ca50c3652fa0b1a5ddbd
Frances Reagan James <adtgwen@gmail.com>
2025-07-30 11:44:46 -0400

    updating UserContext

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import axios from axios;
 import { useAuth } from "./AuthContext";
+import { set } from "mongoose";
 
 // create the context//
 const UserContext = createContext();
@@ -16,3 +17,28 @@ export const UserProvider = ({ childeren }) => {
   const [loading, setLoading] = useState(true);
   const { token, logout } = useAuth();
 }
+
+// check if user is already logged in on app's start//
+useEffect(() => {
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    getCurrentUser();
+  } else {
+    setLoading(false);
+  }
+},[token]);
+
+// is it getCurrentUser or setCurrentUser?//
+const getCurrentUser = async() => {
+  try {
+    const response = await axios.get("/api/users/me");
+    getCurrentUser(response.data);
+  } catch (error) {
+    console.error("Get current user error:", error);
+    // if getting the user fails, logout to clear invalid tokens//
+    logout();
+    getCurrentUser(null);
+  } finally {
+    setLoading(false);
+  }
+  };
