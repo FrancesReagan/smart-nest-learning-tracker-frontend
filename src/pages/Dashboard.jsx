56212588ee45d83df6a445704ef2b041903bb42f
Frances Reagan James <adtgwen@gmail.com>
2025-07-31 23:24:56 -0400

    fixed add course button on dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -215,24 +215,8 @@ const getCourses = useCallback(async () => {
         </div>
       </div>
 
-      <div className="flex space-x-2">
-        <button 
-         type="submit"
-         className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
-         >
-          {editingCourse?"Update Course" : "Add Course"} 
-        </button>
-
-        {editingCourse && (
-          <button
-           type="button"
-           onClick={cancelEdit}
-           className="bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-700"
-           >
-            Cancel
-           </button>
-        )}
-         </div>
+      {/* <div className="flex space-x-2"> */}
+        <button type="submit" className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700">Add Course</button>
       </form>
     </div>
     )}
