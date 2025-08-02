fd67a3e3ad99f707fc866ad8e4cad507beeedf8d
Frances Reagan James <adtgwen@gmail.com>
2025-08-02 16:14:48 -0400

    added confirm button for session delete

--- src/pages/CourseDetail.jsx
+++ src/pages/CourseDetail.jsx
@@ -586,6 +586,23 @@ return (
       </div>
      </div>
   )}
+  {showDeleteCourseModal && (
+    <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
+      <div className="bg-white p-6 rounded-lg shadow-xl text-center">
+        <p className="text-gray-900 text-lg mb-4">Are you sure you want to delete this session?</p>
+        <div className="flex justify-center space-x-4">
+          <button 
+            onClick={handleDeleteSessionConfirmed}
+            className="bg-red-400 text-gray-900 px-4 py-2 rounded hover:bg-red-200 transition duration-300"
+           >
+            Confirm 
+          </button>
+            
+
+            </div>
+      </div>
+    </div>
+  )}
 
        
 
