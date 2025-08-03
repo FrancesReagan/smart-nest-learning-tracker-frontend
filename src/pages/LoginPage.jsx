c83bdea35845faff18712d2b899341b528640d26
Frances Reagan James <adtgwen@gmail.com>
2025-08-03 19:56:21 -0400

    fixing tailwinds css

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -48,7 +48,7 @@ return (
     <div className="absolute inset-0 bg-black/50" />
     <div className="relative z-10 min-h-screen flex items-center justify-center px-6">
       <div className="bg-white/10 backdrop-blur-sm p-8 rounded-lg w-full max-w-md">
-      <h2 className="text-2xl font-bold text-white text-center mb-6">Login</h2>
+      <h2 className="text-5xl font-bold text-white text-center mb-6 drop-shadow-lg">Login</h2>
       
       {error && (
         <div className="bg-red-500/20 border border-red-500 text-red-200 p-3 rounded mb-4">
@@ -58,43 +58,43 @@ return (
 
  <form onSubmit={handleSubmit}>
   <div className="mb-4">
-    <label className="block text-white mb-2">Email</label>
+    <label className="block text-white mb-2 text-2xl drop-shadow-lg">Email</label>
     <input
       type="email"
       value={email}
       onChange={(e) => setEmail(e.target.value)}
-      className="w-full p-3 rounded bg-white/20 text-gray-800 placeholder-gray-300"
+      className="w-full p-3 rounded bg-white/20 text-gray-800 placeholder-gray-900 text-xl"
       placeholder="your@email.com"
       required
       />
   </div>
 
  <div className="mb-6">
-  <label className="block text-white mb-2">Password</label>
+  <label className="block text-white mb-2 text-2xl drop-shadow-lg">Password</label>
   <input
    type="password"
    value={password}
    onChange={(e) => setPassword(e.target.value)}
-   className="w-full p-3 rounded bg-white/20 text-white placeholder-gray-300"
+   className="w-full p-3 rounded bg-white/20 text-white text-xl placeholder-gray-900"
    placeholder="Your Password"
    required
    />
  </div>
 
-<button type="submit" className="w-full bg-blue-600 text-white py-3 rounded hover:bg-blue-700 mb-4 disabled:opacity-50"
+<button type="submit" className="w-full bg-gray-700 text-white text-xl py-3 rounded hover:bg-blue-700 mb-4 disabled:opacity-50 drop-shadow-lg"
     disabled={authLoading}
     >
      {authLoading? "Logging in...": "Login"}
 </button>
  </form>
 <div className="text-center">
-  <p className="text-gray-300">
+  <p className="text-gray-300 text-xl drop-shadow-lg">
     Don't have an account?{" "}
-    <Link to="/register" className="text-blue-400 hover:text-blue-300">
+    <Link to="/register" className="text-blue-300 hover:text-yellow-300">
     Sign Up
     </Link>
   </p>
-  <Link to="/" className="text-gray-400 hover:text-gray-300 text-sm">
+  <Link to="/" className="text-gray-900 hover:text-yellow-300 text-xl">
      Back to home
     </Link>
   </div>
